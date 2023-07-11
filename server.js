5e0ca3632eb7a03c54354b78631931a85b5342c1
thamain1 <114443045+thamain1@users.noreply.github.com>
2023-07-11 00:23:43 -0500

    Update server.js
    updated port

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/api/message', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
